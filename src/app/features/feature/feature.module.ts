0dbf5f429f0275e62cfc676271e271c0e4f5582b
Jonathan Lozano <jlozano@epiclabs.io>
2019-02-10 12:33:11 +0100

    Update to Angular 7

--- src/app/features/feature/feature.module.ts
+++ src/app/features/feature/feature.module.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 // ROUTES
 import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent } from '@ds-features/feature/routes/feature/feature.component';
@@ -13,7 +11,6 @@ import { FeatureComponent } from '@ds-features/feature/routes/feature/feature.co
   ],
   imports: [
     CommonModule,
-    FlexLayoutModule,
     FeatureRoutingModule,
   ],
   providers: [],
